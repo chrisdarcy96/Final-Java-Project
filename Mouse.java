1eb1f907a473eb31f5b0d9b0f8a7ca8f30a56bc6
chrisdarcy96 <chrisdarcy96@gmail.com>
2015-06-04 14:18:11 -0700

    Added comment in html, gave Keegan private vars

--- Mouse.java
+++ Mouse.java
@@ -12,7 +12,7 @@ import java.net.*;
 import java.io.IOException.*;
 
 public class Mouse extends Applet implements MouseListener{
-  Image backGround;
+  private Image backGround;
   public void init(){
     //The following is adding the picture to the background of the panel
     //---------------------------------------------------------
