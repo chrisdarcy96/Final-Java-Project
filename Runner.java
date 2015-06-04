bf2d9ce81ce682fbd4101c714dcaa07e191b2327
chrisdarcy96 <chrisdarcy96@gmail.com>
2015-06-04 09:56:52 -0700

    updated runner

--- Runner.java
+++ Runner.java
@@ -1,9 +1,23 @@
-
+import java.applet.*;
+import java.awt.*;
 
 public class Runner extends Applet{
-
-  public static void main(String[]args){
-    //Do things
+  //private variables
+  private int width;
+  private int height;
+  private Graphics backg;
+  private Image duck=getImage(getDocumentBase(), getParameter("duckspriteactual.png"));
+  public void init(){
+      width=100;
+      height=100;
   }
+  public void paint(Graphics g){
+      g.drawImage(duck, 100, 100, this);
+
+  }
+
+  private int getBirdX(){return -1;}
+  private int getBirdY(){return -1;}
+  private void removeDuck(){}
 
 }
