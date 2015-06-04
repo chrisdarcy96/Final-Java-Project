ee9ba7b8425b84f0831ecb04a341b28fa9aec7d3
chrisdarcy96 <chrisdarcy96@gmail.com>
2015-06-04 13:39:45 -0700

    Created Applet runner, Graphics file is in working order.
    Still requires:
    -correct background picture
    -duck to randomly move across screen
    -mouse clicked event
    --->for now just make sure it registers
    -change cursor symbol to target???

--- Graphics.java
+++ Graphics.java
@@ -1,14 +1,25 @@
 import java.applet.*;
 import java.awt.*;
 
-public class Graphics extends Applet{
+public class Graphics extends Applet implements Runnable{
+  private Image duck;
 
   public void init(){
+    duck=getImage(getDocumentBase(), "duckspriteactual.png");
+    setBackground(Color.white);
+
+  }
+
+  public void run(){
 
   }
 
   public void paint(Graphics g){
 
+  }
+
+  public void update(Graphics g){
+    paint(g);
   }
 
 
