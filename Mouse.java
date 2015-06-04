5af46223e3b5bbc465e4a460120a8a14c3fdc28d
keeganfitz <keeganfitzpatrick@Keegans-Mac-Shared-Files.local>
2015-06-04 09:54:51 -0700

    did a lot of things

--- Mouse.java
+++ Mouse.java
@@ -1,12 +1,25 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
 import java.awt.event.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.util.*;
 
-public class mouse extends Applet implements MouseListener{
+public class Mouse extends Applet implements MouseListener{
   public void init(){
     this.addMouseListener(this);
-    setSize(1000,700);
+    setSize(1400,800);
   }
   public void mouseClicked(MouseEvent e){
-
+    //if((e.getX() >= birdx) && (e.getX() <= birdx+75) && (e.getY() >= birdy) && (e.getY <= birdy+60)){
+      //update score
+      //call levelPassed
+      //call removeDuck
+    //}
   }
   public void mouseEntered(MouseEvent e){
 
