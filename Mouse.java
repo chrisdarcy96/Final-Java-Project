aa0aa60d7792a90e588a951a81b3b0cb34a03628
keeganfitz <keeganfitzpatrick@Keegans-Mac-Shared-Files.local>
2015-06-04 13:33:45 -0700

    Changed the code to allow a background instead of a white background

--- Mouse.java
+++ Mouse.java
@@ -8,11 +8,28 @@ import java.awt.event.*;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.util.*;
+import java.net.*;
+import java.io.IOException.*;
 
 public class Mouse extends Applet implements MouseListener{
+  Image backGround;
   public void init(){
+    //The following is adding the picture to the background of the panel
+    //---------------------------------------------------------
+    //---------------------------------------------------------
+    //It will call the BackGroundPanel class below and that will
+    //override the origional white background of the panel
+    //the image named Duck_Hunt_Lucasta_DA_Wide.png is temporary while abby
+    //finishes the actual background
     this.addMouseListener(this);
     setSize(1400,800);
+    backGround = getImage(getCodeBase(), "Duck_Hunt_Lucasta_DA_Wide.png");
+    BackGroundPanel bgp = new BackGroundPanel();
+    bgp.setLayout(new FlowLayout());
+    bgp.setBackGroundImage(backGround);
+    setLayout(new BorderLayout());
+    add(bgp);
+    
   }
   public void mouseClicked(MouseEvent e){
     //if((e.getX() >= birdx) && (e.getX() <= birdx+75) && (e.getY() >= birdy) && (e.getY <= birdy+60)){
@@ -33,4 +50,27 @@ public class Mouse extends Applet implements MouseListener{
   public void mousePressed(MouseEvent e){
 
   }
+}
+class BackGroundPanel extends Panel {
+     Image backGround;
+
+     BackGroundPanel() {
+          super();
+     }
+
+     public void paint(Graphics g) {
+
+          // get the size of this panel (which is the size of the applet),
+          // and draw the image
+          g.drawImage(getBackGroundImage(), 0, 0,
+              (int)getBounds().getWidth(), (int)getBounds().getHeight(), this);
+     }
+
+     public void setBackGroundImage(Image backGround) {
+          this.backGround = backGround;    
+     }
+
+     private Image getBackGroundImage() {
+          return backGround;    
+     }
 }
